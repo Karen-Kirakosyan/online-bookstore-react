cc85c3bcf0cf256e3caa2f8368758d15f3b11e31
Karen-Kirakosyan <karen.vip.10@gmail.com>
2021-10-07 22:09:40 +0400

    master

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5fdkOYPF59kSzw2PIUso-G-sGCwcRDHU",
+  authDomain: "online-bookstore-2dd16.firebaseapp.com",
+  databaseURL: "https://online-bookstore-2dd16-default-rtdb.firebaseio.com",
+  projectId: "online-bookstore-2dd16",
+  storageBucket: "online-bookstore-2dd16.appspot.com",
+  messagingSenderId: "703293568714",
+  appId: "1:703293568714:web:ec19355aa16f35deaee633"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
