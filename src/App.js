bd6e1c51e8b36838dccb65acb4e5fb75d454e496
Karen-Kirakosyan <karen.vip.10@gmail.com>
2021-10-07 22:18:40 +0400

    aded main router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return "";
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/categories">
+        <Categories />
+      </Route>
+      <Route path="/auth">
+        <Auth />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
